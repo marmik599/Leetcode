de87d3ff66e972ff56b4e8a5a442b2d4079d419b
marmik599 <59131357+marmik599@users.noreply.github.com>
2022-03-01 13:38:39 +0530

    Time: 434 ms (10.58%), Space: 61.5 MB (94.23%) - LeetHub

---
+++ 1497-check-if-array-pairs-are-divisible-by-k/1497-check-if-array-pairs-are-divisible-by-k.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool canArrange(vector<int>& arr, int k)
+    {
+            int n=arr.size();
+            vector <int> freq(k,0);
+           // memset(freq,0,k);
+            for(int i=0;i<n;i++)
+            {
+                int modulo=((arr[i]%k + k)%k);
+                freq[modulo]+=1;
+            }
+        for(int i=0;i<k;i++)
+        {
+            cout<<freq[i]<<" ";
+        }
+            if(freq[0]%2!=0)
+                return false;
+        
+            for(int i=1;i<k;i++)
+            {
+                
+                if(freq[i]!=freq[k-i])
+                    return false;
+            }
+        return true;
+        
+    }
+};
