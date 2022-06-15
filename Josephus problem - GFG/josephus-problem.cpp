62f260d44e77b51d130e7ad68e8bcd66668fb42f
marmik599 <59131357+marmik599@users.noreply.github.com>
2022-06-15 15:19:56 +0530

    Added solution - LeetHub

---
+++ Josephus problem - GFG/josephus-problem.cpp
@@ -0,0 +1,48 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+ // } Driver Code Ends
+/*You are required to complete this method */
+
+class Solution
+{
+    public:
+    int solve(int n,int k)
+    {
+        if(n==1)
+        return 1;
+        int r = (solve(n-1,k) + k)%n;
+        if(r==0)
+        return n;
+        else 
+        return r;
+        
+    }
+    int josephus(int n, int k)
+    {
+       return solve(n,k);
+    }
+};
+
+
+
+// { Driver Code Starts.
+
+int main() {
+	
+	int t;
+	cin>>t;//testcases
+	while(t--)
+	{
+		int n,k;
+		cin>>n>>k;//taking input n and k
+		
+		//calling josephus() function
+		Solution ob;
+		cout<<ob.josephus(n,k)<<endl;
+	}
+	return 0;
+}  // } Driver Code Ends
