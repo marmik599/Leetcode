e9636f020112d2a4c98905f82753db18cdc5a067
marmik599 <59131357+marmik599@users.noreply.github.com>
2022-07-14 22:09:50 +0530

    Added solution - LeetHub

---
+++ Target Sum - GFG/target-sum.cpp
@@ -0,0 +1,67 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//Back-end complete function Template for C++
+
+class Solution {
+  public:
+  int solve(int index,vector<int> a,int tar)
+  {
+      if(index == 0){
+            if(tar==0 && a[0]==0)
+                return 2;
+            if(tar==0 || tar == a[0])
+                return 1;
+            return 0;
+    }
+    int not_taken=solve(index-1,a,tar);
+    int taken=0;
+    if(tar>=a[index])
+        taken=solve(index-1,a,tar-a[index]);
+        
+    return taken+not_taken;
+  }
+    int findTargetSumWays(vector<int>&a ,int target) 
+    {
+            int n=a.size();
+            int totalsum=0;
+            for(int i=0;i<n;i++)
+            {
+                totalsum+=a[i];
+            }
+            if((totalsum - target)<0) return 0;
+            if((totalsum-target)%2 == 1) return 0;
+            int tar= (totalsum-target)/2;
+            
+            return solve(n-1,a,tar);
+    }
+};
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        
+        cin>>N;
+        vector<int>arr(N);
+        
+        for(int i=0 ; i<N ; i++){
+            cin>>arr[i];
+        }
+        int target;
+        cin >> target;
+
+        Solution ob;
+        cout<<ob.findTargetSumWays(arr,target);
+        cout<<"\n";
+    }
+    return 0;
+}  // } Driver Code Ends
