7224f067a22fba6f6ddfc1ba5b57a658e8ce1999
marmik599 <59131357+marmik599@users.noreply.github.com>
2022-08-03 19:21:27 +0530

    Time: 7 ms (7.24%), Space: 6 MB (22.67%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.cpp
+++ 278-first-bad-version/278-first-bad-version.cpp
@@ -7,28 +7,22 @@ public:
     {
         int start=1;
         int end=n;
-        
         while(start<=end)
         {
             int mid=start+(end-start)/2;
             
             if(isBadVersion(mid)==1 && isBadVersion(mid-1)==0)
-            {
                 return mid;
-            }
-            else if(isBadVersion(mid)==0 && isBadVersion(mid+1)==1)
-            {
+            
+            else if(isBadVersion(mid+1)==1 && isBadVersion(mid)==0)
                 return mid+1;
-            }
+            
             else if(isBadVersion(mid)==1)
-            {
                 end=mid-1;
-            }
-            else if(isBadVersion(mid)==0)
-            {
+            else
                 start=mid+1;
-            }
         }
         return -1;
+        
     }
 };
