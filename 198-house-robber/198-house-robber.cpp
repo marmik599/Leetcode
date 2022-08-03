14f885ce05809e092f991952d88f7abd2ca59281
marmik599 <59131357+marmik599@users.noreply.github.com>
2022-08-03 16:38:49 +0530

    Time: 4 ms (36.13%), Space: 9.2 MB (6.02%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
+
+    
     int solve(int index,vector<int> nums,vector<int> &dp)
     {
         if(index==0)
-            return nums[index];
+            return nums[0];
         if(index<0)
             return 0;
-        
         if(dp[index]!=-1)
             return dp[index];
-        
+        int not_taken=0+solve(index-1,nums,dp);
         int taken=nums[index]+solve(index-2,nums,dp);
-        int nottaken=0+solve(index-1,nums,dp);
-        dp[index]=max(taken,nottaken);
-        return dp[index];
+        
+        return dp[index]=max(taken,not_taken);
     }
     int rob(vector<int>& nums) 
     {
